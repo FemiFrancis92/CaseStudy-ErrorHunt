06ef2746c9294213584e4371cf068add2fa67e78
FemiFrancis92 <ffmief@gmail.com>
2021-12-31 18:08:52 +0530

    new commit

---
+++ src/data/user.js
@@ -0,0 +1,24 @@
+var users=[{
+    "id":1,
+    "uid":"aiswaryawalter@gmail.com",
+    "pwd":"aiswarya"
+},
+{
+    "id":2,
+    "uid":"surya@gmail.com",
+    "pwd":"surya"
+},
+{
+    "id":3,
+    "uid":"bazith@gmail.com",
+    "pwd":"bazith"
+},
+{
+    "id":4,
+    "uid":"Ann",
+    "pwd":"ann"
+}
+]
+
+
+module.exports=users;////Part #1 Point 4 changed from user to users
